cb7623bd81ce17be7069f4aa304f1aedeb9634b0
Arnold-XV <arnolduswasesa7@gmail.com>
2025-11-23 12:10:58 +0700

    update(admin): jadwal wawancara

--- frontend/src/modules/admin/components/division-table.tsx
+++ frontend/src/modules/admin/components/division-table.tsx
@@ -92,7 +92,7 @@ const DivisionTable = ({ allUsers, admin }: { allUsers: any; admin: any }) => {
 
                 {/* Tanggal Pilihan Hima */}
                 <td>
-                  {dipilihHima && user.enrolledSlugHima === admin.username ? (
+                  {dipilihHima ? (
                     formatDate(jamHima)
                   ) : (
                     <p className="opacity-50">Belum memilih</p>
@@ -101,7 +101,7 @@ const DivisionTable = ({ allUsers, admin }: { allUsers: any; admin: any }) => {
 
                 {/* Tanggal Pilihan OTI */}
                 <td>
-                  {dipilihOti && user.enrolledSlugOti === admin.username ? (
+                  {dipilihOti ? (
                     formatDate(jamOti)
                   ) : (
                     <p className="opacity-50">Belum memilih</p>
